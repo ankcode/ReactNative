73c0eafd62374a0116bcaa77336ebd624ec4c7f5
Ankur Gupta <ankur.gupta@synfioo.com>
2020-11-29 12:19:08 +0100

    Prepared the file structure and babel module resolver.

--- rssFeedReader/babel.config.js
+++ rssFeedReader/babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@app/navigation': './app/navigation',
+          '@app/screens': './app/screens',
+          '@app/components': './app/components',
+          '@app/api': './app/api',
+          '@app/config': './app/config',
+          '@app/hooks': './app/hooks',
+          '@app/assets': './assets',
+        },
+      },
+    ],
+  ],
 };
